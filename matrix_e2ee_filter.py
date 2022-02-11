a42dbd497391b11986fcb20b85273b34cbdada34
Konstantin Sharlaimov <konstantin.sharlaimov@gmail.com>
2022-02-11 17:07:00 +0100

    Fix room creation bug

--- matrix_e2ee_filter.py
+++ matrix_e2ee_filter.py
@@ -10,19 +10,19 @@ logger = logging.getLogger(__name__)
 #   In addition the plugin will filter out events for enabling encryption on room based on the server:
 #     - deny_encryption_for_users_of: if the event sender is on the server in the list (i.e. @user:example.org)
 #     - deny_encryption_for_rooms_of: if the room is on the server in the list (i.e. #room:example.org)
-#
+
 # third_party_event_rules:
 #   module: "matrix_e2ee_filter.EncryptedRoomFilter"
 #   config:
 #     deny_encryption_for_users_of: ['example.org']
 #     deny_encryption_for_rooms_of: ['example.org']
-#
+
 # You may also want to add the following to your logging config to debug the plugin:
-#
+
 # loggers:
 #     matrix_e2ee_filter:
 #         level: INFO
-#
+
 
 class EncryptedRoomFilter:
 
@@ -59,7 +59,6 @@ class EncryptedRoomFilter:
     async def on_create_room(self, requester: "synapse.types.Requester", request_content: dict, is_requester_admin: bool,) -> None:
         # Cut out encryption setting for the room, force room to be unencrypted
         # Note that this still doesn't block users from enabling encryption at a later stage
-        log.info('%s', requester.get_dict())
         filtered_initial_state = []
         for event in request_content.get('initial_state', []):
             if event['type'] not in ['m.room.encryption']:
