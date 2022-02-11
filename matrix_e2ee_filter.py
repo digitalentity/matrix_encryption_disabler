ee80beedc5084a5fabf3c91d8df6d59457d3a790
Konstantin (DigitalEntity) Sharlaimov <konstantin.sharlaimov@gmail.com>
2022-02-11 20:47:43 +0100

    Migrate to a stable Spam checker callbacks API

--- matrix_e2ee_filter.py
+++ matrix_e2ee_filter.py
@@ -1,6 +1,6 @@
 import logging
 import synapse
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Union
 from synapse import module_api
 
 logger = logging.getLogger(__name__)
@@ -31,12 +31,12 @@ class EncryptedRoomFilter:
         self.deny_user_servers = config.get("deny_encryption_for_users_of", [])
         self.deny_room_servers = config.get("deny_encryption_for_rooms_of", [])
         self.api.register_third_party_rules_callbacks(on_create_room = self.on_create_room,)
-        self.api.register_third_party_rules_callbacks(check_event_allowed = self.check_event_allowed,)
+        self.api.register_spam_checker_callbacks(check_event_for_spam=self.check_event_for_spam,)
         logger.info('Registered custom rule filter: EncryptedRoomFilter')
         logger.info('Deny lists: users of %s; rooms of %s', self.deny_user_servers, self.deny_room_servers)
 
 
-    async def check_event_allowed(self, event: "synapse.events.EventBase", state_events: "synapse.types.StateMap",) -> Tuple[bool, Optional[dict]]:
+    async def check_event_for_spam(self, event: "synapse.events.EventBase") -> Union[bool, str]:
         event_dict = event.get_dict()
         try:
             event_type = event_dict.get('type', None)
@@ -45,15 +45,15 @@ class EncryptedRoomFilter:
                 _, room_server = event_dict['room_id'].split(':', 2)
 
                 if user_server in self.deny_user_servers:
-                    logger.warn('Denied encryption for %s because of requestor', event_dict.get('room_id', '<unknown>'))
-                    return (False, event_dict)
+                    logger.warn('Denied E2EE for %s / requestor', event_dict.get('room_id', '<unknown>'))
+                    return 'Encryption is not allowed'
                 elif room_server in self.deny_room_servers:
-                    logger.warn('Denied encryption for %s because of room server', event_dict.get('room_id', '<unknown>'))
-                    return (False, event_dict)
+                    logger.warn('Denied E2EE for %s / room server', event_dict.get('room_id', '<unknown>'))
+                    return 'Encryption is not allowed'
         except Exception:
-            raise
-            return (False, event_dict)
-        return (True, event_dict)
+            logger.warn('Exception when trying to handle the event: %s', event_dict)
+            return 'Denied because an error occurred when processing the request'
+        return False
 
 
     async def on_create_room(self, requester: "synapse.types.Requester", request_content: dict, is_requester_admin: bool,) -> None:
